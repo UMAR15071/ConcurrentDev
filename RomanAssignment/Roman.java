4b12456edc2fdaf20796761bf873db995fa69786
UMAR15071 <syedmumer15071@gmail.com>
2024-09-16 10:43:08 +0100

    Comments added

--- RomanAssignment/Roman.java
+++ RomanAssignment/Roman.java
@@ -1,3 +1,15 @@
+/*
+ * 
+ * Name: Syed Umar
+ * Student ID: C00278724
+ * 
+ * Logic: The solution in this file convert Roman numbers given in form of Strings converted into integers. The logic is that we will simply 
+ * go through the String and check each character. The character we get represent a number eg: I = 1 and V = 5. We have to make sure
+ * while going through each one of these characters we keep track of the previous character, so that incase we get IV which is equal to 4. When we 
+ * get to I we add 1 but when we get to V instead of adding 5 we will add 3 because our previous character is I which makes IV as 4. This is the 
+ * basic which cover all the possible test cases. 
+ */
+
 class Roman {
     public static void main(String[] args){
         int number = romanNumber("III");
